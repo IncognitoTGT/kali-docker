46216456b17340f72eb47a5bdc7221244ba119f6
MA-TTP <maiyaan@sudormrf.ml>
2023-03-16 08:25:37 -0400

    uwu

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-/usr/share/novnc/utils/novnc_proxy --listen 8081 --vnc localhost:5900 && x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :0 -auth /usr/sbin/lightdm/:0 -bg -ncache -ncache_cr -quiet -forever -create
+/usr/share/novnc/utils/novnc_proxy --listen 8081 --vnc localhost:5900 && x11 vnc -xkb -noxrecord -noxfixes -noxdamage -display :0 -auth /usr/sbin/lightdm/:0 -bg -ncache -ncache_cr -quiet -forever -create
